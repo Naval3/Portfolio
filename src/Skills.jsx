d8379f2cdd14e7aadf3ba9d9679b63fc8516496e
Naval koranga <navalkoranga63@gmail.com>
2023-02-07 16:38:13 +0530

    Changed colors

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -9,9 +9,9 @@ import Typescript from "/src/Typescript.png";
 function Skills() {
   return (
     <div className="bg-blue-300 ">
-      <h2 className="text-5xl text-black pt-5 font-bold text-center">
+      <h1 className="text-3xl text-black pt-5 font-bold text-center">
         My Skills
-      </h2>
+      </h1>
 
       <div className="flex justify-center mt-3 items-center">
         <div className="flex lg:grid grid-cols-3 gap-3 items-center flex-wrap justify-center">
@@ -20,16 +20,10 @@ function Skills() {
             <img src={Html} className="w-full border-md" />
 
             <div className="font-xl p-3">
-              <h2 className="text-center text-xl text-[#0AD6A0]">HTML</h2>
-              <p className="">
+              <h2 className="text-center text-xl text-green-500">HTML</h2>
+              <p className="text-center">
                 I have worked on a wide range of industry level projects using
-                JavaScript.
-              </p>
-              <p>
-                <span className="text-[#0AD6A0] text-xl">Experience - </span>I
-                have an experience of 1 year with MongoDB and I am able to
-                create projects like ecommerce platform using React in MERN
-                Stack.
+                HTML.
               </p>
             </div>
           </div>
@@ -38,16 +32,10 @@ function Skills() {
             <img src={JS} className="w-full border-md" />
 
             <div className="font-xl p-3">
-              <h2 className="text-center text-xl text-[#0AD6A0]">JavaScript</h2>
-              <p>
+              <h2 className="text-center text-xl text-green-500">JavaScript</h2>
+              <p className="text-center">
                 I have worked on a wide range of industry level projects using
-                JavaScript.
-              </p>
-              <p>
-                <span className="text-[#0AD6A0] text-xl">Experience - </span>I
-                have an experience of 1 year with MongoDB and I am able to
-                create projects like ecommerce platform using React in MERN
-                Stack.
+                JS.
               </p>
             </div>
           </div>
@@ -56,16 +44,10 @@ function Skills() {
             <img src={Css} className="w-full border-md" />
 
             <div className="font-xl p-3">
-              <h2 className="text-center text-xl text-[#0AD6A0]">CSS</h2>
-              <p>
+              <h2 className="text-center text-xl text-green-500">CSS</h2>
+              <p className="text-center">
                 I have worked on a wide range of industry level projects using
-                JavaScript.
-              </p>
-              <p>
-                <span className="text-[#0AD6A0] text-xl">Experience - </span>I
-                have an experience of 1 year with MongoDB and I am able to
-                create projects like ecommerce platform using React in MERN
-                Stack.
+                CSS.
               </p>
             </div>
           </div>
@@ -74,34 +56,10 @@ function Skills() {
             <img src={Tailwind} className="w-full border-md" />
 
             <div className="font-xl p-3">
-              <h2 className="text-center text-xl text-[#0AD6A0]">Tailwind</h2>
-              <p>
+              <h2 className="text-center text-xl text-green-500">Tailwind</h2>
+              <p className="text-center">
                 I have worked on a wide range of industry level projects using
-                JavaScript.
-              </p>
-              <p>
-                <span className="text-[#0AD6A0] text-xl">Experience - </span>I
-                have an experience of 1 year with MongoDB and I am able to
-                create projects like ecommerce platform using React in MERN
-                Stack.
-              </p>
-            </div>
-          </div>
-          {/* React Card*/}
-          <div className="bg-white w-72 border border-md rounded-md m-3">
-            <img src={Reacticon} className="w-full border-md" />
-
-            <div className="font-xl p-3">
-              <h2 className="text-center text-xl text-[#0AD6A0]">React</h2>
-              <p>
-                I have worked on a wide range of industry level projects using
-                JavaScript.
-              </p>
-              <p>
-                <span className="text-[#0AD6A0] text-xl">Experience - </span>I
-                have an experience of 1 year with MongoDB and I am able to
-                create projects like ecommerce platform using React in MERN
-                Stack.
+                Taildind.
               </p>
             </div>
           </div>
@@ -110,16 +68,22 @@ function Skills() {
             <img src={Typescript} className="w-full border-md" />
 
             <div className="font-xl p-3">
-              <h2 className="text-center text-xl text-[#0AD6A0]">TypeScript</h2>
-              <p>
+              <h2 className="text-center text-xl text-green-500">TypeScript</h2>
+              <p className="text-center">
                 I have worked on a wide range of industry level projects using
-                JavaScript.
+                TS.
               </p>
-              <p>
-                <span className="text-[#0AD6A0] text-xl">Experience - </span>I
-                have an experience of 1 year with MongoDB and I am able to
-                create projects like ecommerce platform using React in MERN
-                Stack.
+            </div>
+          </div>
+          {/* React Card*/}
+          <div className="bg-white w-72 border border-md rounded-md m-3">
+            <img src={Reacticon} className="w-full border-md" />
+
+            <div className="font-xl p-3">
+              <h2 className="text-center text-xl text-green-500">React</h2>
+              <p className="text-center">
+                I have worked on a wide range of industry level projects using
+                React.
               </p>
             </div>
           </div>
