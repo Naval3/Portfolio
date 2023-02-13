593410bf9b346c7ac69b691a70479e81a6b423a1
Naval koranga <navalkoranga63@gmail.com>
2023-02-13 14:18:04 +0530

    added tic tac toe

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import game from "/src/game.png";
 
 function Projects() {
   return (
@@ -69,18 +70,15 @@ function Projects() {
 
           <div className="grid-cols-2 md:grid">
             <div>
-              <a
-                href="https://assignment-5-navalkoranga.codeyogi-batch-2.repl.co/"
-                target="_blank"
-              >
+              <a href="https://crossnzero.netlify.app/" target="_blank">
                 <img
                   className="transition duration-300 ease-in-out delay-150 border rounded border-cyan-300 hover:-translate-y-1 hover:scale-110"
-                  src="https://media.discordapp.net/attachments/1019293202521538642/1024536668100894830/flex.jpg?width=1001&height=473"
+                  src={game}
                 />
               </a>
             </div>
             <div className="md:ml-4">
-              <h1 className="text-xl font-bold">Flex cards</h1>
+              <h1 className="text-xl font-bold">Tic-tac-toe</h1>
 
               <div className="flex gap-2">
                 <p className="px-2 font-bold text-white bg-blue-900 rounded-md">
@@ -90,8 +88,8 @@ function Projects() {
               </div>
 
               <p>
-                It's just a flex cards project which uses flex and flex wrap
-                property to make a design responsive.
+                It's just a simple tic-tac-toe game.I built this game on
+                React.js with Tailwindcss.
               </p>
             </div>
           </div>
