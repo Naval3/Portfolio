ee357cc36141f7bcda4f0c8be67f2e56560a0b69
Naval koranga <navalkoranga63@gmail.com>
2023-02-14 13:53:02 +0530

    added resume link

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,7 +18,7 @@ function Header() {
           </h1>
 
           <a
-            href="https://drive.google.com/file/d/1Nn0FGCoIRSSwT6jrrtFZhLIoD4m-rmkq/view?usp=sharing"
+            href="https://drive.google.com/file/d/1jq0tB-ZBSW4N_Rt8oBUou7Xl95w56YZt/view?usp=sharing"
             target="_blank"
             className="px-4 py-3 text-center font-semibold hover:text-white bg-red-500 rounded-md hover:bg-red-700"
           >
