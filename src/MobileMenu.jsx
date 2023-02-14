0165f5dbf8efc37a385364ea6da960e093d15cc8
Naval koranga <navalkoranga63@gmail.com>
2023-02-14 19:01:19 +0530

    sticky

--- src/MobileMenu.jsx
+++ src/MobileMenu.jsx
@@ -5,34 +5,34 @@ import { Link } from "react-router-dom";
 function MobileMenu({ menuClose }) {
   return (
     <div onClick={menuClose}>
-      <div className="flex flex-col mb-1 md:hidden translate-y-0">
+      <div className="flex flex-col mb-1 md:hidden translate-y-0 ">
         <Link
-          className="px-4 bg-blue-300 border-t hover:text-green-700 border-y-gray-500"
+          className="px-4 text-center bg-blue-300 border-t hover:text-green-700 border-y-gray-500"
           to="/"
         >
           Home
         </Link>
         <Link
-          className="px-4 bg-blue-300 border-t hover:text-green-700 border-y-gray-500"
+          className="px-4 text-center bg-blue-300 border-t hover:text-green-700 border-y-gray-500"
           to="/about"
         >
           About
         </Link>
         <Link
-          className="px-4 bg-blue-300 border-t hover:text-green-700 border-y-gray-500"
+          className="px-4 text-center bg-blue-300 border-t hover:text-green-700 border-y-gray-500"
           to="/skills"
         >
           Skills
         </Link>
         <Link
-          className="px-4 bg-blue-300 border-t hover:text-green-700 border-y-gray-500"
+          className="px-4 text-center bg-blue-300 border-t hover:text-green-700 border-y-gray-500"
           to="/projects"
         >
           Projects
         </Link>
 
         <Link
-          className="px-4 bg-blue-300 border-t border-b hover:text-green-700 border-y-gray-500"
+          className="px-4 text-center bg-blue-300 border-t border-b hover:text-green-700 border-y-gray-500"
           to="/contact"
         >
           Contact
